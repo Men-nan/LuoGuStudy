bb9f2a79c9cf7b92413168d1c92dcfaa9442ca1b
Men-nan <mennan2022@outlook.com>
2026-09-07 10:33:20 +0800

    输出第二个输入的整数（os：意义何在？）

---
+++ PrintSecondNum.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class PrintSecondNum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        System.out.println(b);
+    }
+}
